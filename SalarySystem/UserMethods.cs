3932c975f4bdd8707d5759b020c347ec24b14701
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-11-10 14:47:44 +0100

    Began implemented VerifyValidCredentials()

--- SalarySystem/UserMethods.cs
+++ SalarySystem/UserMethods.cs
@@ -8,20 +8,34 @@ namespace SalarySystem
         // Test that the properties are correctly set
         public static User CreateUserAccount()
         {
-            //if (username.Any(char.IsLetter) && username.Any(char.IsDigit))
-            //{
-            //    if (password.Any(char.IsLetter) && password.Any(char.IsDigit))
-            //    {
-            //        User user = new User();
-            //        user.Username = username;
-            //        user.Password = password;
-            //        user.Salary = salary;
-            //        user.CompanyRole = role;
+            Console.WriteLine("New username: ");
+            string newUsername = Console.ReadLine();
+            Console.WriteLine("New password: ");
+            string newPassword = Console.ReadLine();
 
-            //        return user;
-            //    }
-            //}
+            VerifyValidCredentials(new)
+
+            if (username.Any(char.IsLetter) && username.Any(char.IsDigit))
+            {
+                if (password.Any(char.IsLetter) && password.Any(char.IsDigit))
+                {
+                    User user = new User();
+                    user.Username = username;
+                    user.Password = password;
+                    user.Salary = salary;
+                    user.CompanyRole = role;
+
+                    return user;
+                }
+            }
             return null;
+        }
+
+        private static bool VerifyValidCredentials(string username, string password)
+        {
+            bool isValid = false;
+
+            return isValid;
         }
 
         // Test that the user's id does not exist in the list afterwards
