bbf7390a2b8570fa8e3436e1503378740a22e8eb
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-11-10 14:47:44 +0100

    Began implemented VerifyValidCredentials()

---
+++ SalarySystemTests/AccountAuthenticationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalarySystem.Tests
+{
+    [TestClass()]
+    public class AccountAuthenticationTests
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LogoutTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
