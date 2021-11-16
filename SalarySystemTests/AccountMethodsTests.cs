02d46ff2155ae15b7d5e7f4f60bd99258430f0ad
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-11-16 16:12:54 +0100

    Integration test coming along

---
+++ SalarySystemTests/AccountMethodsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalarySystem;
+
+namespace SalarySystem.Tests
+{
+    [TestClass()]
+    public class AccountMethodsTests
+    {
+        [TestMethod()]
+        public void AddAccountToAccountsListTest()
+        {
+        }
+    }
+}
--- SalarySystemTests/IntegrationTests.cs
+++ SalarySystemTests/IntegrationTests.cs
@@ -1,34 +1,57 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SalarySystem;
 using SalarySystem.Tests;
+using System;
 
 namespace SalarySystemTests
 {
     [TestClass()]
     public class IntegrationTests
     {
-        //private static User user = new();
+        private static User mockUser = new();
 
-        //[TestInitialize]
-        //public void Startup()
-        //{
-        //    user.Username = "elias";
-        //    user.Password = "hjelm";
-        //    user.CompanyRole = "janitor";
-        //    user.IsOnline = false;
-        //}
+        [ClassInitialize]
+        public static void Startup(TestContext tc)
+        {
+            mockUser.Username = "elias";
+            mockUser.Password = "hjelm";
+            mockUser.CompanyRole = "janitor";
+            mockUser.IsOnline = false;
+        }
 
         /*
           1. Create user
           2. Login user
-          3.
           3. Logout user
         */
-        public static User testUser = new();
+        public static User userPlaceholder = new();
 
         [TestMethod()]
-        public void UserCreationAndLoginAndLogoutTest()
+        public void CreateUserAccountTest_ShouldHaveUniqueId()
         {
+            var expected = mockUser;
+            var actual = UserMethods.CreateUserAccount(Tuple.Create("elias", "hjelm", "janitor"));
+            Assert.AreNotEqual(expected.AccountId, actual.AccountId);
+            userPlaceholder = actual;
+        }
+
+        [TestMethod()]
+        public void LoginTest_ShouldReturnTrue_WhenGivenValidCredentials()
+        {
+            Console.WriteLine($"{userPlaceholder.IsOnline}");
+
+            AccountAuthentication.Login(userPlaceholder, Tuple.Create(userPlaceholder.Username, userPlaceholder.Password));
+
+            Assert.IsTrue(userPlaceholder.IsOnline);
+        }
+
+        [TestMethod()]
+        public void LogoutTest_ShouldReturnFalse_WhenAccountIsNotNullAndIsOnline()
+        {
+            Console.WriteLine($"{userPlaceholder.IsOnline}");
+            AccountAuthentication.Logout(userPlaceholder);
+
+            Assert.IsFalse(userPlaceholder.IsOnline);
         }
     }
 }
